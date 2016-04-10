fea14b3a2738d2b7e87ddab3805e9d8fcfa953bf
E. Bosch <presidev@AT@gmail.com>
2016-04-11 01:05:09 +0200

    Add an option to convert UTF-8 emojis to ASCII emoticons

---
+++ emoji2emoticon.tcl
@@ -0,0 +1,94 @@
+
+# ibotg  IRC bot to Telegram: a bot that allows using Telegram from IRC
+# Copyright (C) 2016 E. Bosch
+
+# MIT/X11 License
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including   
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+set emoji(\xf0\x9f\x98\x80) :D
+set emoji(\xf0\x9f\x98\x81) xD
+set emoji(\xf0\x9f\x98\x82) x_D
+set emoji(\xf0\x9f\x98\x83) :D
+set emoji(\xf0\x9f\x98\x84) xD
+set emoji(\xf0\x9f\x98\x85) x`D
+set emoji(\xf0\x9f\x98\x86) xD
+set emoji(\xf0\x9f\x98\x87) O:)
+set emoji(\xf0\x9f\x98\x88) }:)
+set emoji(\xf0\x9f\x98\x89) ";)"
+set emoji(\xf0\x9f\x98\x8a) x)
+set emoji(\xf0\x9f\x98\x8b) xP
+set emoji(\xf0\x9f\x98\x8c) :)
+set emoji(\xf0\x9f\x98\x8d) E>)
+set emoji(\xf0\x9f\x98\x8e) B)
+set emoji(\xf0\x9f\x98\x8f) "- -,"
+set emoji(\xf0\x9f\x98\x90) :|
+set emoji(\xf0\x9f\x98\x91) :|
+set emoji(\xf0\x9f\x98\x92) "-. -."
+set emoji(\xf0\x9f\x98\x93) :`|
+set emoji(\xf0\x9f\x98\x94) :|
+set emoji(\xf0\x9f\x98\x95) :/
+set emoji(\xf0\x9f\x98\x96) :S
+set emoji(\xf0\x9f\x98\x97) :*
+set emoji(\xf0\x9f\x98\x98) :**
+set emoji(\xf0\x9f\x98\x99) x*
+set emoji(\xf0\x9f\x98\x9a) x*
+set emoji(\xf0\x9f\x98\x9b) :P
+set emoji(\xf0\x9f\x98\x9c) ";P"
+set emoji(\xf0\x9f\x98\x9d) xP
+set emoji(\xf0\x9f\x98\x9e) :(
+set emoji(\xf0\x9f\x98\x9f) :((
+set emoji(\xf0\x9f\x98\xa0) :(
+set emoji(\xf0\x9f\x98\xa1) :(
+set emoji(\xf0\x9f\x98\xa2) :_(
+set emoji(\xf0\x9f\x98\xa3) x(
+set emoji(\xf0\x9f\x98\xa4) :<(
+set emoji(\xf0\x9f\x98\xa5) :`(
+set emoji(\xf0\x9f\x98\xa6) :(|
+set emoji(\xf0\x9f\x98\xa7) :(||
+set emoji(\xf0\x9f\x98\xa8) ||:(
+set emoji(\xf0\x9f\x98\xa9) ::(
+set emoji(\xf0\x9f\x98\xaa) :`(
+set emoji(\xf0\x9f\x98\xab) x(
+set emoji(\xf0\x9f\x98\xac) :E
+set emoji(\xf0\x9f\x98\xad) :__(|
+set emoji(\xf0\x9f\x98\xae) :O
+set emoji(\xf0\x9f\x98\xaf) :o
+set emoji(\xf0\x9f\x98\xb0) :`O
+set emoji(\xf0\x9f\x98\xb1) :O>
+set emoji(\xf0\x9f\x98\xb2) 8-O
+set emoji(\xf0\x9f\x98\xb3) :8|
+set emoji(\xf0\x9f\x98\xb4) :.zz
+set emoji(\xf0\x9f\x98\xb5) 6)
+set emoji(\xf0\x9f\x98\xb6) :
+set emoji(\xf0\x9f\x98\xb7) :W
+set emoji(\xf0\x9f\x98\xb8) >:D
+set emoji(\xf0\x9f\x98\xb9) :_D
+set emoji(\xf0\x9f\x98\xba) >:D
+set emoji(\xf0\x9f\x98\xbb) >E>D
+set emoji(\xf0\x9f\x98\xbc) ">- -,"
+set emoji(\xf0\x9f\x98\xbd) >:*
+set emoji(\xf0\x9f\x98\xbe) >:(
+set emoji(\xf0\x9f\x98\xbf) >:_(
+set emoji(\xf0\x9f\x98\xc0) >:(|
+set emoji(\xf0\x9f\x98\xc1) :(
+set emoji(\xf0\x9f\x98\xc2) :)
+set emoji(\xf0\x9f\x98\xc3) (:
+set emoji(\xf0\x9f\x98\xc4) "o o,"
